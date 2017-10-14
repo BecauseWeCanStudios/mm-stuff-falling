c056571e3d87d71f73f814ad7b01d3f1b1f719b1
GoldFeniks <ggoldenfeniks@gmail.com>
2017-10-14 13:16:06 +1000

    Add basic model

---
+++ stuff_falling/Model.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stuff_falling
+{
+    public static class Model {
+
+        public enum Forces { Gravity, Archimedes, Drag, Viscosity }
+
+        public struct Properties
+        {
+            public List<Forces> Forces;
+            public double Height;
+            public double Mass;
+            public double EndTime;
+            public double SegmentCount;
+            public bool IsConstGravitationalAcceleration;
+            public double Volume;
+            public double ObjectDensity;
+            public double EnviromentDensity;
+            public double ObjectRadius;
+            public double EnviromentViscosity;
+            public double DragCoefficient;
+        }
+
+        public struct Result
+        {
+            public List<double> Height;
+            public List<double> Speed;
+            public List<double> Time;
+        }
+
+        public static void Calculate(Properties properties) {
+            //do stuff
+        }
+
+    }
+}
