09e9b484ec607ca68f3598a5f5384c0d22c27fa1
polikutinevgeny <polikutinevgeny@gmail.com>
2017-10-15 12:31:11 +1000

    Half of GUI is done

--- stuff_falling/MainWindow.xaml.cs
+++ stuff_falling/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,6 +26,85 @@ namespace stuff_falling
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Update() {}
+
+        private void DoubleTBPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            System.Globalization.CultureInfo ci = System.Threading.Thread.CurrentThread.CurrentCulture;
+            string decimalSeparator = ci.NumberFormat.CurrencyDecimalSeparator;
+            if (decimalSeparator == ".")
+            {
+                decimalSeparator = "\\" + decimalSeparator;
+            }
+            var textBox = sender as TextBox;
+            var pos = textBox.CaretIndex;
+            e.Handled = !Regex.IsMatch(textBox.Text.Substring(0, pos) + e.Text + textBox.Text.Substring(pos), @"^[-+]?[0-9]*" + decimalSeparator + @"?[0-9]*$");
+        }
+
+        private void IntTBPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var pos = textBox.CaretIndex;
+            e.Handled = !Regex.IsMatch(textBox.Text.Substring(0, pos) + e.Text + textBox.Text.Substring(pos), @"^[-+]?[0-9]*$");
+        }
+
+        private void TB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.IsLoaded)
+            {
+                Update();
+            }
+        }
+
+        private void TB_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = e.Key == Key.Space;
+        }
+    }
+
+    [ValueConversion(typeof(object), typeof(string))]
+    public class StringFormatConverter : IValueConverter, IMultiValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(new object[] { value }, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            System.Diagnostics.Trace.TraceError("StringFormatConverter: does not support TwoWay or OneWayToSource bindings.");
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string format = parameter?.ToString();
+                if (String.IsNullOrEmpty(format))
+                {
+                    System.Text.StringBuilder builder = new System.Text.StringBuilder();
+                    for (int index = 0; index < values.Length; ++index)
+                    {
+                        builder.Append("{" + index + "}");
+                    }
+                    format = builder.ToString();
+                }
+                return String.Format(/*culture,*/ format, values);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.TraceError("StringFormatConverter({0}): {1}", parameter, ex.Message);
+                return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            System.Diagnostics.Trace.TraceError("StringFormatConverter: does not support TwoWay or OneWayToSource bindings.");
+            return null;
         }
     }
 }
